72813e522bcc78a3d494230199a439bd20727741
Audun Birkenes <audunb97@gmail.com>
2019-04-09 00:01:35 +0200

    laget rammen til h-fil

---
+++ rammeverk/source/queue.h
@@ -0,0 +1,11 @@
+//These functions dictate the logic behind the queue-system
+
+//2019, Audun Birkenes og Jonas Rogde Jørgensen
+
+#ifndef __INCLUDE_DRIVER_H__
+#define __INCLUDE_DRIVER_H__
+
+
+
+
+#endif
