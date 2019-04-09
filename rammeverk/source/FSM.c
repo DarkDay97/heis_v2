e7b3e6b3de3d4cd70d4b895a1d0d33c3ff1e700d
andraurl <andreas@aurlien.net>
2019-04-09 19:48:58 +0200

    La til logikk for endring av lys i FSM_state_machine().

--- rammeverk/source/FSM.c
+++ rammeverk/source/FSM.c
@@ -2,6 +2,7 @@
 #include "elev.h"
 #include "timer.h"
 #include "queue.h"
+#include "lights.h"
 
 #include <stdio.h>
 
@@ -20,7 +21,8 @@ static FSM_states_t current_state = FLOOR_CLOSED;
 //Gir posisjonen i 0-3. -1 er ugyldig verdi.
 static int prev_pos = -1;
 
-static int current_pos = -1;
+//Midlertidig variabel for å forhindre feilavlesning fra etasjesensor
+static int temp_pos = -1;
 
 //Holder rede på posisjonene mellom etasjer. 1-3 for alle mellomtilstander
 static int pos_between = 0;
@@ -33,6 +35,7 @@ void FSM_system_init(){
     while(1){
         if(elev_get_floor_sensor_signal() == 0){
             elev_set_motor_direction(DIRN_STOP);
+            elev_set_door_open_lamp(0);
             current_state = FLOOR_CLOSED;
             prev_pos = 0;
             break;
@@ -50,18 +53,26 @@ void FSM_update_pos_between(elev_motor_direction_t dir, int pos){
 
 void FSM_state_machine(){
 
-    queue_take_order();
+    lights_update_ordering_lights();
+
+    elev_set_stop_lamp(elev_get_stop_signal());
     
-    printf("prev_pos: %d\n", prev_pos);
+    if(!(elev_get_stop_signal())){
+        queue_take_order();
+        lights_set_lights();
+    }
+
+    //printf("prev_pos: %d\n", prev_pos);
 
     switch (current_state)
     {
         case FLOOR_CLOSED:
             if (elev_get_stop_signal()){
+                elev_set_door_open_lamp(1);
                 current_state = FLOOR_OPEN;
                 break;
             } else if (queue_have_orders()){
-                prev_dir = queue_get_order(prev_dir, prev_pos, 0);
+                prev_dir = queue_calculate_direction(prev_dir, prev_pos, 0);
                 if(prev_dir){
                     elev_set_motor_direction(prev_dir);
                     FSM_update_pos_between(prev_dir, prev_pos);
@@ -69,6 +80,7 @@ void FSM_state_machine(){
                     break;
                 } else {
                     timer_reset();
+                    elev_set_door_open_lamp(1);
                     current_state = FLOOR_OPEN;
                     break;
                 }
@@ -78,30 +90,41 @@ void FSM_state_machine(){
             break;
 
         case FLOOR_OPEN:
-            queue_delete_order(elev_get_floor_sensor_signal());
+            if(queue_get_order(prev_pos)){
+                timer_reset();
+            }
+            queue_delete_order(prev_pos);
+            lights_reset_lights(prev_pos);
             if (elev_get_stop_signal()){
                 timer_reset();
                 queue_delete_all_orders();
+                lights_reset_all_lights();
                 break;
-            } else if (timer_expired()){
+            }
+            if (timer_expired()){
+                elev_set_door_open_lamp(0);
                 current_state = FLOOR_CLOSED;
                 break;
             }
+            
             break;
 
         case MOVING:
-            current_pos = elev_get_floor_sensor_signal();
+            temp_pos = elev_get_floor_sensor_signal();
             if (elev_get_stop_signal()){
                 elev_set_motor_direction(DIRN_STOP);
                 queue_delete_all_orders();
+                lights_reset_all_lights();
                 current_state = STATIONARY;
                 break;
             }
-            if (current_pos + 1){
-                prev_pos = current_pos;
+            if (temp_pos + 1){
+                prev_pos = temp_pos;
+                elev_set_floor_indicator(prev_pos);
                 if (queue_should_stop_at_floor(prev_dir, prev_pos)){
                     elev_set_motor_direction(DIRN_STOP);
                     timer_reset();
+                    elev_set_door_open_lamp(1);
                     current_state = FLOOR_OPEN;
                 } else {
                     FSM_update_pos_between(prev_dir, prev_pos);
@@ -113,8 +136,9 @@ void FSM_state_machine(){
         case STATIONARY:
             if (elev_get_stop_signal()){
                 queue_delete_all_orders();
+                lights_reset_all_lights();
             } else if (queue_have_orders()){
-                prev_dir = queue_get_order(prev_dir, prev_pos, pos_between); 
+                prev_dir = queue_calculate_direction(prev_dir, prev_pos, pos_between); 
                 elev_set_motor_direction(prev_dir);
                 current_state = MOVING;
             }
