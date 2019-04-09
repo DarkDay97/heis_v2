c8f10baef055d39a7a9e63c554336fa728d044d6
andraurl <andreas@aurlien.net>
2019-04-09 18:11:05 +0200

    Stokket om på rekkefølge av handlinger i tilstander

--- rammeverk/source/FSM.c
+++ rammeverk/source/FSM.c
@@ -19,6 +19,10 @@ static FSM_states_t current_state = FLOOR_CLOSED;
 
 //Gir posisjonen i 0-3. -1 er ugyldig verdi.
 static int prev_pos = -1;
+
+static int current_pos = -1;
+
+//Holder rede på posisjonene mellom etasjer. 1-3 for alle mellomtilstander
 static int pos_between = 0;
 
 static elev_motor_direction_t prev_dir = DIRN_STOP;
@@ -48,22 +52,25 @@ void FSM_state_machine(){
 
     queue_take_order();
     
-    printf("current state: %d\n", current_state);
+    printf("prev_pos: %d\n", prev_pos);
 
     switch (current_state)
     {
         case FLOOR_CLOSED:
             if (elev_get_stop_signal()){
                 current_state = FLOOR_OPEN;
+                break;
             } else if (queue_have_orders()){
                 prev_dir = queue_get_order(prev_dir, prev_pos, 0);
                 if(prev_dir){
                     elev_set_motor_direction(prev_dir);
                     FSM_update_pos_between(prev_dir, prev_pos);
                     current_state = MOVING;
+                    break;
                 } else {
                     timer_reset();
                     current_state = FLOOR_OPEN;
+                    break;
                 }
             } else if (!queue_have_orders()){
                 prev_dir = DIRN_STOP;
@@ -72,27 +79,34 @@ void FSM_state_machine(){
 
         case FLOOR_OPEN:
             queue_delete_order(elev_get_floor_sensor_signal());
-
-            if (timer_expired()){
-                current_state = FLOOR_CLOSED;
-            } else if (elev_get_stop_signal()){
+            if (elev_get_stop_signal()){
                 timer_reset();
                 queue_delete_all_orders();
+                break;
+            } else if (timer_expired()){
+                current_state = FLOOR_CLOSED;
+                break;
             }
             break;
 
         case MOVING:
-            if (elev_get_floor_sensor_signal() + 1){
-                if (queue_should_stop_at_floor(prev_dir, elev_get_floor_sensor_signal())){
-                    prev_pos = elev_get_floor_sensor_signal();
-                    elev_set_motor_direction(DIRN_STOP);
-                    timer_reset();
-                    current_state = FLOOR_OPEN;
-                }
-            } else if (elev_get_stop_signal()){
+            current_pos = elev_get_floor_sensor_signal();
+            if (elev_get_stop_signal()){
                 elev_set_motor_direction(DIRN_STOP);
                 queue_delete_all_orders();
                 current_state = STATIONARY;
+                break;
+            }
+            if (current_pos + 1){
+                prev_pos = current_pos;
+                if (queue_should_stop_at_floor(prev_dir, prev_pos)){
+                    elev_set_motor_direction(DIRN_STOP);
+                    timer_reset();
+                    current_state = FLOOR_OPEN;
+                } else {
+                    FSM_update_pos_between(prev_dir, prev_pos);
+                    // oppdater etasjelys
+                }
             }
             break;
 
@@ -100,7 +114,7 @@ void FSM_state_machine(){
             if (elev_get_stop_signal()){
                 queue_delete_all_orders();
             } else if (queue_have_orders()){
-                prev_dir = queue_get_order(prev_dir, prev_pos, pos_between); //Funker ikke nødvendigvis som den skal
+                prev_dir = queue_get_order(prev_dir, prev_pos, pos_between); 
                 elev_set_motor_direction(prev_dir);
                 current_state = MOVING;
             }
