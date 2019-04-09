1fc742588cbb852a653b8504e4bc55abbc94aab5
Audun Birkenes <audunb97@gmail.com>
2019-04-09 04:43:13 +0200

    fjerna forhåndslagte programmet i main og la til kill-switch med obstruksjonsbryter

--- rammeverk/source/main.c
+++ rammeverk/source/main.c
@@ -1,4 +1,5 @@
 #include "elev.h"
+#include "FSM.h"
 #include <stdio.h>
 
 
@@ -9,20 +10,11 @@ int main() {
         return 1;
     }
 
-    printf("Press STOP button to stop elevator and exit program.\n");
-
-    elev_set_motor_direction(DIRN_UP);
+   
 
     while (1) {
-        // Change direction when we reach top/bottom floor
-        if (elev_get_floor_sensor_signal() == N_FLOORS - 1) {
-            elev_set_motor_direction(DIRN_DOWN);
-        } else if (elev_get_floor_sensor_signal() == 0) {
-            elev_set_motor_direction(DIRN_UP);
-        }
-
-        // Stop elevator and exit program if the stop button is pressed
-        if (elev_get_stop_signal()) {
+        // Stop elevator and exit program if the obstruction signal is high
+        if (elev_get_obstruction_signal()) {
             elev_set_motor_direction(DIRN_STOP);
             break;
         }
