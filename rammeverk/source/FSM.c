16edf64f2e2aa23882daeb8128ad33a16f1937dd
andraurl <andreas@aurlien.net>
2019-04-09 16:24:08 +0200

    Fjernet enum for mellom etasjer og satte static int istedet. La inn logikk for å sette igang bevegelse etter nødstopp. Endret på queue_get_order() - sine parameter og funksjonalitet

--- rammeverk/source/FSM.c
+++ rammeverk/source/FSM.c
@@ -14,18 +14,12 @@ typedef enum {
     TEST
 } FSM_states_t;
 
-typedef enum {
-    NONE,
-    ONE_TWO, 
-    TWO_THREE, 
-    THREE_FOUR
-}FSM_between_floor_pos_t;
 
 static FSM_states_t current_state = FLOOR_CLOSED;
 
 //Gir posisjonen i 0-3. -1 er ugyldig verdi.
 static int prev_pos = -1;
-static FSM_between_floor_pos_t pos_between = NONE;
+static int pos_between = 0;
 
 static elev_motor_direction_t prev_dir = DIRN_STOP;
 
@@ -62,7 +56,7 @@ void FSM_state_machine(){
             if (elev_get_stop_signal()){
                 current_state = FLOOR_OPEN;
             } else if (queue_have_orders()){
-                prev_dir = queue_get_order(prev_dir, prev_pos);
+                prev_dir = queue_get_order(prev_dir, prev_pos, 0);
                 if(prev_dir){
                     elev_set_motor_direction(prev_dir);
                     FSM_update_pos_between(prev_dir, prev_pos);
@@ -106,7 +100,7 @@ void FSM_state_machine(){
             if (elev_get_stop_signal()){
                 queue_delete_all_orders();
             } else if (queue_have_orders()){
-                prev_dir = queue_get_order(prev_dir, prev_pos); //Funker ikke nødvendigvis som den skal
+                prev_dir = queue_get_order(prev_dir, prev_pos, pos_between); //Funker ikke nødvendigvis som den skal
                 elev_set_motor_direction(prev_dir);
                 current_state = MOVING;
             }
